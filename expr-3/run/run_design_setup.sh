f2f6d4fe9f19f6430691bde9114d9ef061d868ba
fpy <fupeiyi@foxmail.com>
2026-06-17 20:50:21 +0800

    expr-3: remove unnecessary netlist copy to run/

--- expr-3/run/run_design_setup.sh
+++ expr-3/run/run_design_setup.sh
@@ -5,11 +5,12 @@ mkdir -p ../logs ../design_data
 cd "$(dirname "$0")"
 rm -rf ../cpu_pad.mw
 
-cp ../design_data/cpu_pad_netlist.v . 2>/dev/null || {
+# Ensure design_data has the required input files
+if [ ! -f ../design_data/cpu_pad_netlist.v ]; then
     echo "Copying netlist from expr-2..."
     cp ../../expr-2/syn/mapped/cpu_pad_netlist.v ../design_data/
     cp ../../expr-2/syn/mapped/cpu_pad.sdc ../design_data/
-}
+fi
 
 icc_shell -f ../scripts/design_setup.tcl 2>&1 | tee ../logs/design_setup.log
 echo "=> design_setup done, log: logs/design_setup.log"
